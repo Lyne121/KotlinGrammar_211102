e379f2cb0b481ce5e2c322b174590fe81e374231
leejiwon <irne000805@gmail.com>
2021-12-06 17:39:37 +0900

    Initial commit

---
+++ app/src/main/java/com/practicejw/kotlingrammar_211102/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.practicejw.kotlingrammar_211102
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnVariable01.setOnClickListener {
+//            기본적인 변수 만들고, 대입하고, 활용 (로그찍기)
+
+//            1. 변수 공간만 만들기
+            val myName : String
+            var myHeight : Double
+
+//            2. 변수에 데이터 대입
+            myName = "마이네임"
+            myHeight = 153.5
+
+//            3. 데이터 활용
+            Log.d("이름", myName)
+            Log.d("키", myHeight.toString()) // Log의 재료는 String만 대입 가능 => 변수를 쓸 때 자료형을 신경써야함
+
+//            참고 - 모든 변수는 '참조형 변수'이며, 부가 기능들이 있다
+//              공통 기능 : String으로 변환 가능 => toString()
+        }
+    }
+}
